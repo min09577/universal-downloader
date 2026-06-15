02d64bfd02f81e0b8f9d8354c257b53400e918d4
min09577 <min09577@users.noreply.github.com>
2026-06-16 01:42:31 +0800

    🔧 Fix CI: local.properties, PYTHON_PATH, stable Chaquopy version

--- standalone-android/app/build.gradle.kts
+++ standalone-android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.chaquo.python") version "16.0.0"
+    id("com.chaquo.python") version "15.0.1"
 }
 
 android {
